fa08a7e0ee74c8322cb7b3cded6017cdfb0733a6
AbhinavChaudhary11 <abhinavchaudhary1103@gmail.com>
2025-04-30 00:48:29 +0530

    Temporarily renamed Cards folder to fix case issue

--- frontend/src/components/Dashboard/ExpenseTransactions.jsx
+++ frontend/src/components/Dashboard/ExpenseTransactions.jsx
@@ -1,7 +1,7 @@
 import moment from "moment";
 import React from "react";
 import { LuArrowRight } from "react-icons/lu";
-import TransactionInfoCard from "../cards/TransactionInfoCard";
+import TransactionInfoCard from "../Cards_temp/TransactionInfoCard";
 
 const ExpenseTransactions = ({transactions, onSeeMore}) => {
   return (
